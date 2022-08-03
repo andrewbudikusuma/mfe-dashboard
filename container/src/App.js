a47d884da3e2e6bc2f2f5b4962b4f53aefedd221
Andrew Budikusuma <andrew.budikusuma@gmail.com>
2022-08-04 01:12:34 +0700

    container init

---
+++ container/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './app.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default () => {
+  return <h1 className='text-2xl text-red-700'>hi There</h1>
+};
