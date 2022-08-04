904b4f4c5306fd9a23ec9cf489f3dbde461f6ae1
Andrew Budikusuma <andrew.budikusuma@gmail.com>
2022-08-04 15:56:51 +0700

    add header container

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
 import AuthApp from './components/AuthApp';
 import './app.css';
 
 export default () => {
   return <div>
-    <h1 className='text-2xl text-red-700'>hi There Container</h1>
-    <hr />
+    <Header />
     <AuthApp />
   </div>
 };
---
+++ container/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+
+  return (
+    <div className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <span className="sr-only">Logo App</span>
+            <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
+          </div>
+          <div className="flex items-center justify-end md:flex-1 lg:w-0">
+            <button className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign in </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
