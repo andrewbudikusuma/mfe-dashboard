3d847ac0390cc5c245498ee7920c036ab2856a84
Andrew Budikusuma <andrew.budikusuma@gmail.com>
2022-08-04 15:57:05 +0700

    revamp sign in and forgot password

--- auth/src/components/Forgot.js
+++ auth/src/components/Forgot.js
@@ -3,11 +3,35 @@ import { Link } from 'react-router-dom';
 
 export default function Forgot() {
   return (
-    <div>
-      <h1 className='text-2xl text-blue-300 font-sans'>Hi There Forgot in!</h1>
-      <Link to="/">
-        <button className='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>go to sign in</button>
-      </Link>
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Forgot Password</h2>
+        </div>
+        <form className="mt-8 space-y-6">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">Email address</label>
+              <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              <Link to="/" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Sign in
+              </Link>
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Reset Password
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
