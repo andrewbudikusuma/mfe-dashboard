f53f50754aadfddb28341dbaeb47bd1b6d0c27e2
Andrew Budikusuma <andrew.budikusuma@gmail.com>
2022-08-11 14:35:21 +0700

    communicating through callbacks

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -5,8 +5,9 @@ import { createMemoryHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+  history.listen(onNavigate);
   const root = createRoot(el);
 
   root.render(<App history={history} />);
