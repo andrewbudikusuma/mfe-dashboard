8c4093c240c52cd33a626fb753aaef1a544fcac0
Andrew Budikusuma <andrew.budikusuma@gmail.com>
2022-08-11 11:00:34 +0700

    add browser router into container app

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from './components/Header';
 import AuthApp from './components/AuthApp';
 import './app.css';
@@ -7,9 +7,9 @@ import './app.css';
 console.log('here there')
 export default () => {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <AuthApp />
-    </div>
+    </BrowserRouter>
   )
 };
